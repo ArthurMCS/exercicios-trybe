dce410bbc88538ab3df758a49b6d8578c13ed15a
Arthur Moreira <arthurmcs96@gmail.com>
2022-03-08 21:02:05 -0300

    cria a rota get /book/:id

--- back-end/bloco-24-Node-js-ORM-e-autenticação/dia-01-ORM-interface-da-aplicacao-com-o-banco-de-dados/controllers/bookController.js
+++ back-end/bloco-24-Node-js-ORM-e-autenticação/dia-01-ORM-interface-da-aplicacao-com-o-banco-de-dados/controllers/bookController.js
@@ -10,4 +10,15 @@ const getAll = async (_req, res) => {
     }
 }
 
-module.exports = { getAll }
+const getById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const book = await Book.findByPk(id);
+      return res.status(200).json(book);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({message: 'algo deu errado'});
+    }
+  };
+
+module.exports = { getAll, getById }
--- back-end/bloco-24-Node-js-ORM-e-autenticação/dia-01-ORM-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-Node-js-ORM-e-autenticação/dia-01-ORM-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -8,5 +8,6 @@ const bookController = require('./controllers/bookController');
 app.use(bodyParser.json());
 
 app.get('/books', bookController.getAll);
+app.get('/books/:id', bookController.getById);
 
 app.listen(() => console.log(`Ouvindo na porta 3000!`));
