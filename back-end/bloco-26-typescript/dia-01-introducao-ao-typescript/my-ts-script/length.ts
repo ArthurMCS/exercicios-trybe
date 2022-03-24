696cfe3fabe3c643036679f4c108bb2b553d246b
Arthur Moreira <arthurmcs96@gmail.com>
2022-03-24 13:42:38 -0300

    exercicio 01 do dia 01 do bloco 26

---
+++ back-end/bloco-26-typescript/dia-01-introducao-ao-typescript/my-ts-script/length.ts
@@ -0,0 +1,23 @@
+const unidades = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
+
+const error = (unidade: string): any => {
+    console.error(`A ${unidade} não é uma unidade!`)
+    return;
+};
+
+const convert = (valor: number, unidadeBase: string, unidadeConversao: string): number => {
+
+    if(!unidades.includes(unidadeBase)) return error(unidadeBase);
+    if(!unidades.includes(unidadeConversao)) return error(unidadeConversao);
+
+    const unidadeBIndex = unidades.indexOf(unidadeBase);
+    const unidadeCIndex = unidades.indexOf(unidadeConversao);
+
+    const expoente = (unidadeCIndex - unidadeBIndex);
+
+    console.log(valor * Math.pow(10, expoente))
+    return valor * Math.pow(10, expoente);
+    
+};
+
+convert(1, 'km', 'cm');
